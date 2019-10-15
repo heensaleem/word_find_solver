06a3e6e574dc4ce41ad568ea5d5f4d62b48ff677
heensaleem <heenakouser440@gmail.com>
2019-10-15 14:49:25 -0500

    Word find componet shows up on DOM

---
+++ build/precache-manifest.9fd6ec1a1d29d7b4e8afeedb73b57c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9b91cc485437f44f3fc7",
+    "url": "/static/js/main.6ba03050.chunk.js"
+  },
+  {
+    "revision": "bb30a959db73ccc53558",
+    "url": "/static/js/2.43decef4.chunk.js"
+  },
+  {
+    "revision": "9b91cc485437f44f3fc7",
+    "url": "/static/css/main.51232582.chunk.css"
+  },
+  {
+    "revision": "b0258725f91f64a981d31cc20b7c462f",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/WordFind/WordFind.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WordFind extends Component {
+
+  render(){
+    return (
+    <div>
+        <p>Word Finder goes here</p>
+    </div>
+    );
+  }
+}
+
+export default WordFind;
