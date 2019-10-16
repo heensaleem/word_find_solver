851619618e589e9bfe08271ed40c5053e45a563e
heensaleem <heenakouser440@gmail.com>
2019-10-16 13:59:45 -0500

    Dom setup

--- src/components/WordFind/WordFind.js
+++ src/components/WordFind/WordFind.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
 class WordFind extends Component {
+   
+  state = {
+    inputString : ''
+  }
 
+  handleChange = (event) => {
+    console.log('in handleChange', event.target.value);
+    this.setState({
+        inputString: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('in handleSubmit', this.state.inputString );
+    
+  }
   render(){
+
     return (
-    <div>
-        <p>Word Finder goes here</p>
-    </div>
+      <div>
+      <h2>Word Finder Search</h2>
+      <pre>{JSON.stringify(this.state)}</pre>
+      <form onSubmit={this.handleSubmit}>
+          <input type='text'  onChange={this.handleChange} />
+          <input type='submit' value='Find Words' />
+
+      </form>
+  </div>
     );
   }
 }
